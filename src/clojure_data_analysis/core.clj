4d40af82cbe885083e4bd5174f5e11667a788e06
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-02-01 13:20:43 +0900

    add fib

--- src/clojure_data_analysis/core.clj
+++ src/clojure_data_analysis/core.clj
@@ -1,6 +1,7 @@
 (ns clojure-data-analysis.core
   (:require [clojure.string :as str]
             [clojure.java.io :as io]
+            [criterium.core :as q]
             [clojure.core.reducers :as r]))
 ;  (:require [clojure.string :as str :refer [replace]]
 
@@ -23,6 +24,55 @@
   [texts]
   (map #(str/split % #"\s+") texts))
 
+(defn fizzbuzz
+  [n]
+  (cond
+    (= (mod n 15) 0) "FizzBuzz"
+    (= (mod n 3) 0) "Fizz"
+    (= (mod n 5) 0) "Buzz"
+    :else n))
+                                        ; (q/quick-bench (map #(map (fn [e] (fizzbuzz e)) %) (partition-all 500000 (range 1 1000000))))
 
+(defn rand-point [] [(rand) (rand)])
 
+(defn center-dist [[x y]]
+  (Math/sqrt (+ (* x x) (* y y))))
 
+(defn count-in-circle
+  [n]
+  (->> (repeatedly n rand-point) (map center-dist) (filter #(<= % 1.0)) count))
+
+(defn mc-pi
+  [n]
+  (* 4.0 (/ (count-in-circle n) n)))
+
+(defn in-circle-flag
+  [p]
+  (if (<= (center-dist p) 1.0) 1 0))
+
+(defn mc-pi-pmap
+  [n]
+  (let [in-circle (->> (repeatedly n rand-point)
+                       (pmap in-circle-flag)
+                       (reduce + 0))]
+    (* 4.0 (/ in-circle n))))
+
+(defn mc-pi-part
+  ([n] (mc-pi-part 512 n))
+  ([chunk-size n]
+   (let [step (int
+               (Math/floor (float (/ n chunk-size))))
+         remainder (mod n chunk-size)
+         parts (lazy-seq
+                (cons remainder
+                      (repeat step chunk-size))) ; (chunk-size chunk-size ....)
+         in-circle (reduce + 0
+                           (pmap count-in-circle
+                                 parts))]
+     (* 4.0 (/ in-circle n)))))
+
+(def chunk-size 4096)
+
+(def input-size 1000000)
+
+(def fib-seq (lazy-cat [0 1] (map + (rest fib-seq) fib-seq)))
